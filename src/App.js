e3e8e452cd502c013f97cdfb8bb87bc61bf9fea0
ukaron <serious1nikos@gmail.com>
2020-12-24 11:21:21 +0300

    change url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
         <h1>todo: <h2>create design</h2>
+            <h2>create nav</h2>
             <h2>create components</h2>
             <h2>create animation</h2>
             <h2>add info and jobs</h2>
