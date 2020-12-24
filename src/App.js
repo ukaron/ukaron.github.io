50f2b189223567d513cfef1d95a3bf2c6173d4a7
ukaron <serious1nikos@gmail.com>
2020-12-24 10:58:41 +0300

    add todo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>todo: <h2>create design</h2>
+            <h2>create components</h2>
+            <h2>create animation</h2>
+            <h2>add info and jobs</h2>
+        </h1>
+    </div>
+  );
+}
+
+export default App;
